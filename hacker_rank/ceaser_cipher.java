ec24aefe453374c08ffc5f6947c2aa3257b445c7
Arunvasuacchu <arunvasur@gmail.com>
2024-06-02 12:41:18 +0530

    oh my lsh issue

---
+++ hacker_rank/ceaser_cipher.java
@@ -0,0 +1,11 @@
+package hacker_rank;
+
+public class ceaser_cipher {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = Math.max(a, b);
+        System.out.println(c);
+    }
+}
